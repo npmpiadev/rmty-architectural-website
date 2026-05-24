69681892e30d30f332e4a6670d3e2b861c71576f
Sophia T. <146542886+sopiyaaaaa@users.noreply.github.com>
2026-05-24 18:55:55 +0800

    Fix storage image serving on hostinger

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\StorageController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/storage/{path}', [StorageController::class, 'show'])
+    ->where('path', '.+')
+    ->name('storage.show');
 
 Route::get('/{any?}', function () {
     return view('app');
