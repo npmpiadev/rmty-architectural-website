c758b6b2ca767073b3ea0ca6ea8adca49f10bcbc
Sophia T. <146542886+sopiyaaaaa@users.noreply.github.com>
2026-05-24 19:01:05 +0800

    Remove FINALS_REACT_PROJECT

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\StorageController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/storage/{path}', [StorageController::class, 'show'])
-    ->where('path', '.+')
-    ->name('storage.show');
 
 Route::get('/{any?}', function () {
     return view('app');
